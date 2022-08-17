413656ba55c9df7192f48ac29499dbf82aa97ea0
Leonardo Mercado <lamercadob1@gmail.com>
2022-08-16 19:40:50 -0500

    clase 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { enviroments } from './enviroments';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, ConfigModule.forRoot({
+    envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+    isGlobal: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
+import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class AppService {
+  constructor(
+    private config: ConfigService,
+  ) {};
   getHello(): string {
-    return 'Hello World!';
+    const apikey= this.config.get<String>('API_KEY');
+    const db= this.config.get('DATABASE_NAME');
+    return `Hola: ${apikey}  ${db}`;
   }
 }
